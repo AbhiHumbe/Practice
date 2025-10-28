e1de94e109f5579d938f04f2104dc814e8a28a51
AbhiHumbe <abhihumbe13@gmail.com>
2025-10-29 01:09:05 +0530

    variable.js is added to Practice repo

---
+++ variable.js
@@ -0,0 +1,53 @@
+// let a = 10;
+
+// function testscope(){
+//     let a = 20;
+//     if(true){
+//         let a = 30;
+//         console.log(a);
+        
+//     }
+//     console.log(a);
+// }
+// testscope();
+// console.log(a);
+
+// function test() {
+//   console.log(x);
+//   var x = 5;
+//   console.log(x);
+// }
+
+// test();
+// getName();
+// console.log(x);
+// console.log(getName);
+
+// let x = 7;
+
+// function getName(){
+//     console.log("Namste javaScript");
+    
+// }
+
+// let a = {
+//     name : "Abhi",
+// }
+// let b = a;
+
+// b.name = "Aaysha";
+// console.log(a);
+// console.log(b);
+
+// let a = null + 1;
+// console.log(a);
+
+// let value = "100";
+// value = value - 20;
+// console.log(value, typeof value)
+// value = value + '50';
+// console.log(value, typeof value);
+
+console.log(5 == "5");
+console.log(5 === "5");
+
