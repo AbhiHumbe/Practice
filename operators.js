0c3424656be23e2ff4c2f961eb7ac1157d34d3e8
AbhiHumbe <abhihumbe13@gmail.com>
2025-10-29 11:47:12 +0530

    Adding operators.js for records

---
+++ operators.js
@@ -0,0 +1,49 @@
+// let x = 10;
+// let y = 20;
+
+// if (x>5 && y<25){
+//     console.log("A");
+    
+// }else {
+//     console.log("B")
+// }
+
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if (isAdmin || isLoggedIn){
+//     console.log("Access Granted");
+    
+// }else{
+//     console.log("Access Denied");
+    
+// }
+
+// let temp = 35;
+
+// if (( temp < 30)){
+//     console.log("Hot");
+    
+// }else{
+//     console.log("Pleasent");
+    
+// }
+
+// let temp1 = 35;
+
+// if (!( temp1 < 30)){
+//     console.log("Hot");
+    
+// }else{
+//     console.log("Pleasent");
+    
+// }
+
+// let score = 50;
+
+// let grade = score >= 90 ? "A" : score >=80 ? "B": score <= 60 ? "D" : "fail"; 
+// console.log(grade);
+
+let m = 10;
+console.log(m--);
+console.log(m);
